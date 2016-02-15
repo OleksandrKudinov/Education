451c8119b85dedd722a11395bcab43679c20b01b
OleksandrKudinov <oleksandr.kudinov.1993@gmail.com>
2016-02-15 18:36:17 +0200

    Added Bubble sorting;

---
+++ Education/Education.Cmd/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Education.Core.Algorithms.Sorting;
+
+namespace Education.Cmd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = new int[] {5, 2, 7, 4, 9, 1, 0, 6, 3, 8};
+            var ordered = array.Bubble(x=>x);
+
+            foreach (var value in ordered)
+            {
+                Console.Write("{0}, ",value);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Education/Education.Core/Algorithms/Sorting/BubbleSort.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Education.Core.Algorithms.Sorting
+{
+    public static class BubbleSort
+    {
+        public static IEnumerable<T> Bubble<T, TParam>(this IEnumerable<T> data, Func<T, TParam> extractor) where TParam : IComparable
+        {
+            var array = data.ToArray();
+
+            int max = array.Length;
+            
+            TParam param1, param2;
+            T tmp;
+            
+            while (max != 0)
+            {
+                max--;
+                for (int i = 0; i < max; i++)
+                {
+                    param1 = extractor(array[i]);
+                    param2 = extractor(array[i + 1]);
+
+                    if (param1.CompareTo(param2) > 0)
+                    {
+                        tmp = array[i];
+                        array[i] = array[i + 1];
+                        array[i + 1] = tmp;
+                    }
+                }
+            }
+
+            return array;
+        }
+    }
+}
